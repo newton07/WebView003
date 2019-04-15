e24a923a2c8828bd92fb53ee2e48ecd31a9205ad
DESKTOP-STKSNSV\newton <HGH00501@nifty.com>
2019-04-15 14:32:33 +0900

    Backボタン無効化

--- app/src/main/java/jp/coocan/la/webview003/MainActivity.java
+++ app/src/main/java/jp/coocan/la/webview003/MainActivity.java
@@ -5,27 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
+        mWebView = (WebView) findViewById(R.id.webview01);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("https://www.yahoo.co.jp/");
     }
 
     @Override
@@ -48,5 +47,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d("MainActivity", "keyCode:" + event.getKeyCode());
+        // 端末の戻るボタンでブラウザバック
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mWebView.canGoBack()) {
+                mWebView.goBack();
+            }
+            return true;
+        }
+
+        if(keyCode == KeyEvent.KEYCODE_HOME) {
+            Log.d("MainActivity", "keyCode: HOME");
+            return true;
+        }
+        return super.onKeyDown(keyCode,  event);
     }
 }
