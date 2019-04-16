fa1a12720619cfd963921c9e748c233dce714bd4
DESKTOP-STKSNSV\newton <HGH00501@nifty.com>
2019-04-16 14:04:24 +0900

    修正②

--- app/src/main/java/jp/coocan/la/webview003/MainActivity.java
+++ app/src/main/java/jp/coocan/la/webview003/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl("https://www.yahoo.co.jp/");
-        
     }
 
     @Override
